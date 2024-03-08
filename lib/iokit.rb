90718ef0827044cfd9258b0ba672ff20c14c6716
John DeSilva <desilvjo@umich.edu>
2024-03-07 20:42:48 -0500

    Genesis commit

---
+++ lib/iokit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "iokit/version"
+
+module Iokit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/iokit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Iokit
+  VERSION = "0.1.0"
+end
