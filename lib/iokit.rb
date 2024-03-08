c17c0fc77256b656010656622cdb04b70b13df44
John DeSilva <desilvjo@umich.edu>
2024-03-07 20:59:45 -0500

    Extend FFI::Library

--- lib/iokit.rb
+++ lib/iokit.rb
@@ -7,4 +7,8 @@ require_relative "iokit/version"
 # This module is a wrapper for a subset of the IOKit framework.
 module Iokit
   class Error < StandardError; end
+
+  extend FFI::Library
+
+  ffi_lib "/System/Library/Frameworks/IOKit.framework/IOKit"
 end
