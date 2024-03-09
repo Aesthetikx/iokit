2e0cca4d4d8ff9d338628e0a6ca4b2579c2a176e
John DeSilva <desilvjo@umich.edu>
2024-03-09 10:39:22 -0500

    Define IORegistryEntryCreateIterator

--- lib/iokit.rb
+++ lib/iokit.rb
@@ -4,6 +4,14 @@ require "ffi"
 
 require_relative "iokit/version"
 
+module FFI
+  module Library
+    def define(name, value)
+      define_singleton_method(name) { value }
+    end
+  end
+end
+
 # This module is a wrapper for a subset of the IOKit framework.
 module Iokit
   class Error < StandardError; end
@@ -12,7 +20,21 @@ module Iokit
 
   ffi_lib "/System/Library/Frameworks/IOKit.framework/IOKit"
 
-  # rubocop:disable Style/Documentation
+  # rubocop:disable Layout/LineLength, Style/SymbolArray
+  typedef :uint32,      :mach_port_t
+  typedef :mach_port_t, :io_object_t
+  typedef :io_object_t, :io_registry_entry_t
+  typedef :int,         :kern_return_t
+  typedef :uint32,      :IOOptionBits
+  typedef :string,      :io_name_t
+
+  attach_variable :kIOMasterPortDefault, :mach_port_t
+
+  define :kIOUSBPlane, "IOUSB"
+
+  enum :io_option_bits, [:kIORegistryIterateRecursively, 0x00000001,
+                         :kIORegistryIterateParents,     0x00000002]
+
   class IOUSBDevRequest < FFI::Struct
     layout :bmRequestType, :uint8,
            :bRequest,      :uint8,
@@ -23,12 +45,7 @@ module Iokit
            :wLenDone,      :uint32
   end
 
-  typedef :uint32,      :mach_port_t
-  typedef :mach_port_t, :io_object_t
-  typedef :io_object_t, :io_registry_entry_t
-
-  attach_variable :kIOMasterPortDefault, :mach_port_t
-
   attach_function :IORegistryGetRootEntry, [:mach_port_t], :io_registry_entry_t
-  # rubocop:enable Style/Documentation
+  attach_function :IORegistryEntryCreateIterator, [:io_registry_entry_t, :io_name_t, :IOOptionBits, :pointer], :kern_return_t
+  # rubocop:enable Layout/LineLength, Style/SymbolArray
 end
